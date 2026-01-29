42c9f5ba6c0b15efa78d7b2e8e3bef762590ee7f
Alexander Witzke <alex.witzke@googlemail.com>
2026-01-29 17:03:23 +0100

    update printer

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+PRINTER_NAME_A4="HP2015DN_A4"
 PRINTER_NAME_A4_DUPLEX="HP2015DN_A4_Duplex"
 PRINTER_NAME_A6_MANUAL="HP2015DN_A6_Manual"
 PRINTER_URI="socket://10.0.0.165:9100"
@@ -13,6 +14,23 @@ sleep 3
 until lpstat -r >/dev/null 2>&1; do
   sleep 1
 done
+
+# A4 Drucker
+if ! lpstat -p "$PRINTER_NAME_A4" >/dev/null 2>&1; then
+  echo "Richte Drucker $PRINTER_NAME_A4 ein..."
+  lpadmin \
+    -p "$PRINTER_NAME_A4" \
+    -E \
+    -v "$PRINTER_URI" \
+    -o media=A4 \
+    -o InputSlot=Upper \
+    -m "$PRINTER_PPD"
+
+  lpadmin -p "$PRINTER_NAME_A4" -o OptionDuplex=False
+
+  cupsenable "$PRINTER_NAME_A4"
+  cupsaccept "$PRINTER_NAME_A4"
+fi
 
 # A4 Duplex Drucker
 if ! lpstat -p "$PRINTER_NAME_A4_DUPLEX" >/dev/null 2>&1; then
